b15c4b3cd650dca1a72afae5cee1ac283e7c58a6
matahir01 <77771228+matahir01@users.noreply.github.com>
2026-09-17 23:03:30 +0100

    Expose automated LM1 grillage search workflow

--- src/rc_bridge/workflow/__init__.py
+++ src/rc_bridge/workflow/__init__.py
@@ -4,6 +4,12 @@ from rc_bridge.workflow.grillage_verification_export import (
     GrillageVerificationLoadCase,
     build_project_grillage_verification_model,
 )
+from rc_bridge.workflow.lm1_grillage_search import (
+    ProjectNativeLM1GrillageSearchResult,
+    build_governing_lm1_search_verification_packages,
+    generate_lm1_search_placements,
+    run_project_native_lm1_grillage_search,
+)
 from rc_bridge.workflow.verification_export import (
     build_moving_train_snapshot_verification_model,
     build_project_continuous_verification_model,
@@ -13,7 +19,11 @@ __all__ = [
     "GrillagePointLoad",
     "GrillageSectionProperties",
     "GrillageVerificationLoadCase",
+    "ProjectNativeLM1GrillageSearchResult",
+    "build_governing_lm1_search_verification_packages",
     "build_moving_train_snapshot_verification_model",
     "build_project_continuous_verification_model",
     "build_project_grillage_verification_model",
+    "generate_lm1_search_placements",
+    "run_project_native_lm1_grillage_search",
 ]
